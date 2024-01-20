97b0d8505f71bea6c48a470eb6d0118f6eb3ce8f
Snap-Trap <111644953+Snap-Trap@users.noreply.github.com>
2024-01-20 10:48:41 +0000

    Added some features for the groundcheck and added a scene for testing only
    Ground check is not working yet, but improved the tags and began with importing the tilemap. I also made a scene for testing if stuff works, the other test scene "level 1" is where we will make the level itself

---
+++ Assets/Scripts/Ground_Check.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Ground_Check : MonoBehaviour
+{
+    private bool grounded = false;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private bool IsGrounded()
+    {
+        if (grounded);
+        {
+            return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                IsGrounded();
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                grounded = false;
+            }
+        }
+    }
+}
